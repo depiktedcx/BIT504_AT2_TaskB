247a2b5b8faf101bfaf528ec1b96d17f71b8e9c2
depiktedcx <cnaifnaxcz@outlook.com>
2021-10-28 10:39:11 +1300

    added dotted line to pongpanel

--- src/pong/PongPanel.java
+++ src/pong/PongPanel.java
@@ -1,18 +1,46 @@
 package pong;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
+
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.BasicStroke;
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 	private static final Color WINDOW_BACKGROUND = Color.BLACK;
+	private static final int TIMER_DELAY = 5;
 	
 	public PongPanel() {
 		setBackground(WINDOW_BACKGROUND);
+		Timer timer = new Timer (TIMER_DELAY, this);
+		timer.start();
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		paintDottedLine(g);
+	}
+	
+	private void paintDottedLine(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g.create();
+		Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] {9}, 0);
+		g2d.setStroke(dashed);
+		g2d.setPaint(Color.WHITE);
+		g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+		g2d.dispose();
+	}
+
+	private void update() {
+		
 	}
 	
 	@Override
@@ -35,8 +63,8 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		// TODO Auto-generated method stub
-		
+		update();
+		repaint();
 	}
 
 }
