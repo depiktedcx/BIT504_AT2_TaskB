37f4990c57c65c65a3c6cd76a7a878afb8e45500
depiktedcx <cnaifnaxcz@outlook.com>
2021-10-28 10:13:56 +1300

    window init

--- src/pong/Pong.java
+++ src/pong/Pong.java
@@ -1,9 +1,21 @@
 package pong;
 
-public class Pong {
+import javax.swing.JFrame;
+
+public class Pong extends JFrame {
+	private static final int WINDOW_WIDTH = 800;
+	private static final int WINDOW_HEIGHT = 600;
+	private static final String WINDOW_TITLE = "Pong";
 
 	public static void main(String[] args) {
-		System.out.println("test");
+		new Pong();
+	}
+	public Pong() {
+		setTitle(WINDOW_TITLE);
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
 }
